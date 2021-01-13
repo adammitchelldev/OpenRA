6649776ba23b5019d8a786fd1e5420d7f9f8223f
Adam Mitchell <adammitchelldev@gmail.com>
2021-01-13 12:13:23 +0000

    Remove braces for single-line consequents

--- OpenRA.Game/Server/Server.cs
+++ OpenRA.Game/Server/Server.cs
@@ -1096,9 +1096,7 @@ namespace OpenRA.Server
 								serverGame.SlowDown(amount);
 							}
 							else
-							{
 								Log.Write("server", "Received request to slow down from client {0} but not using new netcode!", conn.PlayerIndex);
-							}
 
 							break;
 						}
@@ -1106,13 +1104,9 @@ namespace OpenRA.Server
 					case "Loaded":
 						{
 							if (LobbyInfo.GlobalSettings.UseNewNetcode)
-							{
 								conn.Loaded = true;
-							}
 							else
-							{
 								Log.Write("server", "Received Loaded message from client {0} but not using new netcode!", conn.PlayerIndex);
-							}
 
 							break;
 						}
@@ -1337,9 +1331,7 @@ namespace OpenRA.Server
 					State = ServerState.GameLoading;
 				}
 				else
-				{
 					State = ServerState.GameStarted;
-				}
 
 				// TODO remove: this "pre-disconnecting" method adds unnecessary complexity just for the sake of the replay stream
 				/*var disconnectData = new[] { (byte)OrderType.Disconnect };
